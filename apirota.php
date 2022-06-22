ab07e8d3951bef08c1d57ce34125efc6c249fed5
Alex Schavareto <schavareto@yahoo.com.br>
2022-06-21 21:35:23 -0300

    Feito alguns ajustes, para teste

--- apirota.php
+++ apirota.php
@@ -1,4 +1,5 @@
 <?php
+    function chamarapi(){
     $request = '{
         "reid": "28223",
         "cep": "38755000"
@@ -18,8 +19,31 @@
     curl_setopt_array($ch, $curlOptions);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-    curl_setopt($ch,CURLOPT_XOAUTH2_BEARER, '79a9e5b3b26a793f0b7d3c596dafb6ded48fa7c2');
-    $rota = json_decode(curl_exec($ch));;
+    curl_setopt($ch,CURLOPT_XOAUTH2_BEARER, 'fdf1ba26392963c5310f57f29fc7cfcc92e6d56c');
+    $rota = json_decode(curl_exec($ch));
+    echo $rota->descricao; 
+   
+  }
+  function gerarToken(){
+  $request = '{"grant_type": "password","username": "hidrogenio-api","password": "UaN8q@uun"}';
+    $curlOptions = [
+        CURLOPT_URL => 'https://apis.totalexpress.com.br/ics-seguranca/v1/oauth2/tokenGerar',
+        CURLOPT_POST => true,
+        CURLOPT_HTTPHEADER => [
+            'Authorization: Basic SUNTOnRvdGFs',
+            'Content-Type: application/json',
+            'x-li-format: json'
+        ],
+        CURLOPT_POSTFIELDS => $request,
+    ];
+    
+    $ch = curl_init();
+    curl_setopt_array($ch, $curlOptions);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $token = json_decode(curl_exec($ch));
+    $ch = curl_close($ch);
+    echo $token->access_token;
+  }
 ?>
 
 <!doctype html>
@@ -43,7 +67,16 @@
 <h1>Teste API</h1>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-<?php echo $rota->descricao; ?>
+ <form method="post" action="apirota.php">
+    <input type=submit required  name='insert' value=save>
+</form>
+
+
+ <?php
+  if (isset($_POST['insert'])){
+    gerarToken();
+  }
+ ?>
 
 
   </body>
